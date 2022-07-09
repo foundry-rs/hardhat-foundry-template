7de5dc89a005d6828f49ba0c9727622b8b3bd716
Michael Santore <msantore@gmail.com>
2022-07-09 07:59:47 -0700

    add deploy script

---
+++ scripts/run.js
@@ -0,0 +1,25 @@
+const main = async () => {
+  const [deployer] = await hre.ethers.getSigners();
+  const accountBalance = await deployer.getBalance();
+
+  console.log("Deploying contracts with account: ", deployer.address);
+  console.log("Account balance: ", accountBalance.toString());
+
+  const waveContractFactory = await hre.ethers.getContractFactory("TokenManagerETH");
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+
+  console.log("WavePortal address: ", waveContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
